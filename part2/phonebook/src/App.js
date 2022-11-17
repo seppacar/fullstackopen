0e0955c829fb4c417ea6adc5c0797b917e912a41
seppacar <sepp.acar@gmail.com>
2022-11-18 00:35:34 +0300

    completed part2

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,18 +4,36 @@ import personService from "./services/persons";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 import Filter from "./components/Filter";
+import Notification from "./components/Notification";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newPerson, setNewPerson] = useState({ name: "", number: "", id: 0 });
   const [filter, setFilter] = useState("");
+  const [notification, setNotification] = useState({ message: "", type: "" });
 
+  // Pull phonebook data from json-server
+  // and store it to the "persons" variable
   useEffect(() => {
-    personService.getAll().then((initialPersons) => {
-      setPersons(initialPersons);
-    });
+    personService
+      .getAll()
+      .then((initialPersons) => {
+        setPersons(initialPersons);
+      })
+      .catch((error) => {
+        setNotification({
+          message: `Cannot render list ${error.message}`,
+          type: "error",
+        });
+        setTimeout(() => {
+          setNotification({ message: "", type: "" });
+        }, 5000);
+      });
   }, []);
 
+  // Add a new person to the phonebook if input name is not already been in the phonebook
+  // if name is present in the list ask user to update the related phone number
+  // then re-render persons list
   const addPerson = (event) => {
     event.preventDefault();
     const checkName = (obj) => obj.name === newPerson.name;
@@ -36,6 +54,24 @@ const App = () => {
               person.id !== response.id ? person : response
             );
             setPersons(updatedPersons);
+          })
+          .then(() => {
+            setNotification({
+              message: `Phone number of ${newPerson.name} updated`,
+              type: "success",
+            });
+            setTimeout(() => {
+              setNotification({ message: "", type: "" });
+            }, 5000);
+          })
+          .catch(() => {
+            setNotification({
+              message: `${newPerson.name} already been deleted from server`,
+              type: "error",
+            });
+            setTimeout(() => {
+              setNotification({ message: "", type: "" });
+            }, 5000);
           });
       }
       return;
@@ -44,22 +80,71 @@ const App = () => {
       name: newPerson.name,
       number: newPerson.number,
     };
-    personService.create(personObject).then((returnedPerson) => {
-      setPersons(persons.concat(returnedPerson));
-      setNewPerson({ name: "", number: "" });
-    });
+    personService
+      .create(personObject)
+      .then((returnedPerson) => {
+        setPersons(persons.concat(returnedPerson));
+        setNewPerson({ name: "", number: "" });
+      })
+      .then(() => {
+        setNotification({
+          message: `${newPerson.name} added to the phonebook`,
+          type: "success",
+        });
+        setTimeout(() => {
+          setNotification({ message: "", type: "" });
+        }, 5000);
+      })
+      .catch((error) => {
+        setNotification({
+          message: `Cannot add ${newPerson.name} to the list due to ${error.message}`,
+          type: "error",
+        });
+        setTimeout(() => {
+          setNotification({ message: "", type: "" });
+        }, 5000);
+      });
   };
 
+  // Delete a person from the phonebook
   const destroyPerson = (id, name) => {
     if (window.confirm(`Do you really want to delete ${name}`)) {
-      personService.destroy(id).then(setPersons(persons.filter((person) => {return (person.id !== id)})));
+      personService
+        .destroy(id)
+        .then(
+          setPersons(
+            persons.filter((person) => {
+              return person.id !== id;
+            })
+          )
+        )
+        .then(() => {
+          setNotification({
+            message: `${name} deleted from the list`,
+            type: "success",
+          });
+          setTimeout(() => {
+            setNotification({ message: "", type: "" });
+          }, 5000);
+        })
+        .catch((error) => {
+          setNotification({
+            message: `Cannot delete ${name} to the list due to ${error.message}`,
+            type: "error",
+          });
+          setTimeout(() => {
+            setNotification({ message: "", type: "" });
+          }, 5000);
+        });
     }
   };
 
+  // Handle new person form changes
   const handleChange = (event) => {
     const { name, value } = event.target;
     setNewPerson({ ...newPerson, [name]: value });
   };
+  // Handle filter query changes
   const handleFilter = (event) => {
     setFilter(event.target.value);
   };
@@ -67,9 +152,8 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-
+      <Notification message={notification.message} type={notification.type} />
       <Filter handleFilter={handleFilter} />
-
       <h3>Add a new</h3>
       <PersonForm
         addPerson={addPerson}
