278dad93aef38ae1de9504ac54832ba51132c6d2
seppacar <sepp.acar@gmail.com>
2022-12-22 19:45:28 +0300

    exercises until 4.14

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -12,5 +12,23 @@ blogsRouter.post('/', async (request, response) => {
   response.status(201).json(resp)
 })
 
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const body = request.body
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  }
+  await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+  const updatedBlog = await Blog.findById(request.params.id)
+  response.json(updatedBlog)
+})
+
 module.exports = blogsRouter
 
--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -3,14 +3,14 @@ const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blog')
-const initial_notes = require('../utils/blog_helper').listWithManyBlogs
+const initials_blogs = require('../utils/blog_helper').listWithManyBlogs
 
 beforeEach(async () => {
   await Blog.deleteMany({})
   let blogObject = {}
-  
-  for (let i = 0; i < initial_notes.length; i++) {
-    blogObject = new Blog(initial_notes[i])
+
+  for (let i = 0; i < initials_blogs.length; i++) {
+    blogObject = new Blog(initials_blogs[i])
     await blogObject.save()
   }
 })
@@ -25,7 +25,7 @@ test('blogs are returned as json', async () => {
 test('all blogs are returned', async () => {
   const response = await api.get('/api/blogs')
 
-  expect(response.body).toHaveLength(initial_notes.length)
+  expect(response.body).toHaveLength(initials_blogs.length)
 })
 
 test('unique identifier property of the blog posts is named id', async () => {
@@ -48,11 +48,11 @@ test('new blog post created successfully in db', async () => {
     .send(newBlog)
     .expect(201)
     .expect('Content-Type', /application\/json/)
-    
+
   const response = await api.get('/api/blogs')
   const titles = response.body.map((r) => r.title)
 
-  expect(response.body).toHaveLength(initial_notes.length + 1)
+  expect(response.body).toHaveLength(initials_blogs.length + 1)
   expect(titles).toContain('Test Title')
 })
 
@@ -81,21 +81,49 @@ test('check if server responds with "400 Bad Request" if "title" or "url" proper
     author: 'Test Author',
     likes: 5,
   }
-  await api
-    .post('/api/blogs')
-    .send(blogWithNoTitle)
-    .expect(400)
-  await api
-    .post('/api/blogs')
-    .send(blogWithNoUrl)
-    .expect(400)
+  await api.post('/api/blogs').send(blogWithNoTitle).expect(400)
+  await api.post('/api/blogs').send(blogWithNoUrl).expect(400)
 })
 
-test('a specific blog is within the returned blogs', async () => {
+test('check if a specific blog is within the returned blogs', async () => {
   const response = await api.get('/api/blogs')
   const contents = response.body.map((r) => r.title)
 
   expect(contents).toContain('Go To Statement Considered Harmful')
+})
+
+test('check if a specific blog with id is deleted', async () => {
+  const blogToDelete = initials_blogs[0]
+  await api.delete(`/api/blogs/${blogToDelete._id}`).expect(204)
+
+  const response = await api.get('/api/blogs')
+  const blogsAtEnd = response.body
+  expect(blogsAtEnd).toHaveLength(initials_blogs.length - 1)
+
+  const contents = blogsAtEnd.map((r) => r.id)
+
+  expect(contents).not.toContain(blogToDelete._id)
+})
+
+test('check if specific blog post updated succesfully', async () => {
+  const blogToUpdate = initials_blogs[0]
+  const updateValue = {
+    title: 'New Title',
+    author: 'New Author',
+    url: 'newurl',
+    likes: 0,
+    id: blogToUpdate._id,
+  }
+  await api
+    .put(`/api/blogs/${blogToUpdate._id}`)
+    .send(updateValue)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+  const blogsAtEnd = response.body
+
+  expect(blogsAtEnd).toContainEqual(updateValue)
 })
 
 afterAll(() => {
